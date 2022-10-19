27204cf7482108c9b95ad4b0f226f961c9dc6efa
kssseeeniaaa <nekakk@mail.ru>
2022-10-19 20:11:15 +0300

    4.1. Разбор первой программы. [#159033 #353030]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -9,6 +9,7 @@ public class Multiple {
         System.out.println("1*6=6");
         System.out.println("1*7=7");
         System.out.println("1*8=8");
+        System.out.println("1*9=9");
 
     }
 }
