f3544a099315fa370b9b25534619edef644e6f49
kssseeeniaaa <nekakk@mail.ru>
2022-11-08 20:14:33 +0300

    5.7. Ипотека [#156315 #358807]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -5,7 +5,7 @@ public class Mortgage {
         int year = 0;
         while (amount > 0) {
             amount += amount * percent / 100 - salary;
-            year += 1;
+            year++;
         }
         return year;
     }
