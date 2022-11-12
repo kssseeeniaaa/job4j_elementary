f3a247d9b4a0e87f949b27e69afbbc01b03a1e49
kssseeeniaaa <nekakk@mail.ru>
2022-11-13 02:06:15 +0300

    6.1. Классический поиск перебором. [#33489]

---
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class FindLoop {
+    public static int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        if (el > data.length) {
+            return rst;
+        }
+        return rst;
+    }
+}
---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FindLoopTest {
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] data = new int[] {5, 10, 3};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenArrayHasNot10ThenMinus1() {
+        int[] data = new int[] {5, 1, 3};
+        int el = 10;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenArrayHas10Then1() {
+        int[] data = new int[] {5, 10, 2};
+        int el = 10;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+}
