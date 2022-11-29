8172d03127a57275e25fd984eccd145285049d42
kssseeeniaaa <nekakk@mail.ru>
2022-11-29 20:53:50 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#34741]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -2,12 +2,11 @@ package ru.job4j.array;
 
 import java.util.Arrays;
 
-public class Machine { //купюра из кармана; //цена покупки
+public class Machine {
     public static int[] change(int money, int price) {
-        int[] coins = {1, 5, 2, 10}; //массив монет в автомате
-        int[] rsl = new int[100]; //сдача в виде массива монет
-        int size = 0; //размер массива rsl, которого он достигнет
-        // после того, как автомат отдаст всю сдачу
+        int[] coins = {1, 5, 2, 10};
+        int[] rsl = new int[100];
+        int size = 0;
         int[] max = SortSelected.sortforMax(coins);
         int sum = money - price;
         for (int index = 0; index < max.length; index++) {
